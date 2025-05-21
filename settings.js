758d642c8d09c2cf6d22b7a565d23d45d3e979fa
RaaiiXD <162945884+RaaiiXD@users.noreply.github.com>
2025-05-21 01:46:30 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '+2348120979160', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Stephen Rai', // Your name
+  ownerNumber: '2348120979160', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
